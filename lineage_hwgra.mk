be18db28f209fb6fa1776d74587e265784349da8
T1mpp4 <timi.rautamaki@gmail.com>
2018-05-22 20:38:45 +0300

    Update lineage_hwgra.mk
    Change-Id: I127b5eb490dde3b38ae930d376eb8712aeb032ed

--- lineage_hwgra.mk
+++ lineage_hwgra.mk
@@ -17,6 +17,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit from hwgra device
 $(call inherit-product, device/huawei/hwgra/device.mk)
