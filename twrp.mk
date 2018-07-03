cc137cea197a1c6bb7b75eecfc053ec97b3140d0
T1mpp4 <timi.rautamaki@gmail.com>
2018-07-03 18:43:28 +0300

    Update TWRP
    Change-Id: I02a37c4e377a1be84b723259e5e55374c85061f9

--- twrp.mk
+++ twrp.mk
@@ -1,4 +1,4 @@
-TWHAVE_SELINUX := false
+TWHAVE_SELINUX := true
 BOARD_TOUCH_RECOVERY := true
 TW_THEME := portrait_hdpi
 TW_NO_BATT_PERCENT := false
@@ -10,19 +10,19 @@ TW_NO_CPU_TEMP := false
 TW_HAS_NO_RECOVERY_PARTITION := false
 TW_HAS_NO_BOOT_PARTITION := false
 TWRP_EVENT_LOGGING := false
-#BOARD_CUSTOM_GRAPHICS := $(LOCAL_PATH)/recovery/twrp-graphics.c
-STE_HARDWARE := true #Not sure if needed
 TW_BRIGHTNESS_PATH := /sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness
 TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
 TWRP_INCLUDE_LOGCAT := true
 TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 150
 TWRP_INCLUDE_LOGCAT := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#in vendor recovery it is RGB565
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"  
-#TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888" 
+TW_EXCLUDE_SUPERSU := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.hi3635
 #this is required to avoid unresponsive touch at boot until screen timeout 
 TW_SCREEN_BLANK_ON_BOOT := true 
 RECOVERY_SDCARD_ON_DATA := true
+
+# Paths
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
