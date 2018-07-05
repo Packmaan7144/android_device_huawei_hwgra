1d10d2ded894ca5d3a803aef2fe331add8fc53e8
Eray Rafet <erayrafet@gmail.com>
2018-07-05 22:25:14 +0300

    Fix display issue and initial F2FS support
    Missing quotation marks, he he...

--- twrp.mk
+++ twrp.mk
@@ -10,8 +10,8 @@ TW_NO_CPU_TEMP := false
 TW_HAS_NO_RECOVERY_PARTITION := false
 TW_HAS_NO_BOOT_PARTITION := false
 TWRP_EVENT_LOGGING := false
-TW_BRIGHTNESS_PATH := /sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness
-TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness"
+TW_CUSTOM_BATTERY_PATH := "/sys/class/power_supply/Battery"
 TWRP_INCLUDE_LOGCAT := true
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 150
@@ -26,3 +26,6 @@ RECOVERY_SDCARD_ON_DATA := true
 # Paths
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
+# Initial F2FS support props
+TARGET_USERIMAGES_USE_F2FS := true
