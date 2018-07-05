02962ff58e6753727fb01507a5101223ea55c399
T1mpp4 <timi.rautamaki@gmail.com>
2018-07-05 17:17:23 +0300

    More permissions
    Change-Id: Ia66e6a2970c37d8b712194fa4d362843d12db1aa

--- device.mk
+++ device.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	libshim \
     libshim_icu \
-    libshim_camera \
+    libshim_other_camera \
     libhw_log \
     libshim_log \
     libshim_algo \
