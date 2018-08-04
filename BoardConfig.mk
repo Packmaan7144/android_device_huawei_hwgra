e47d881eb3b2108b90451a5992cb028acbc1256d
T1mpp4 <timi.rautamaki@gmail.com>
2018-08-04 17:31:30 +0300

    Fix for prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 LOCAL_PATH := device/huawei/hwgra
 
 # OTA ASSERT
